cd679f85a79a97301e1a1f9ff2495a64e41ddb26
cw <c.wagner.germany@gmail.com>
2026-08-12 16:46:22 +0200

    contact

---
+++ src/app/components/contact-form/contact-form.ts
@@ -0,0 +1,65 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-contact-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './contact-form.html',
+  styleUrl: './contact-form.scss',
+})
+export class ContactForm {
+
+  private fb = inject(FormBuilder);
+
+  submittedSuccessfully = false;
+
+  private readonly emailRegex =
+    /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+
+  contactForm = this.fb.nonNullable.group({
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(2)
+      ]
+    ],
+
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(this.emailRegex)
+      ]
+    ],
+
+    topic: [
+      'general',
+      Validators.required
+    ]
+  });
+
+  onSubmit(): void {
+    this.submittedSuccessfully = false;
+
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(
+      'Formulardaten:',
+      this.contactForm.getRawValue()
+    );
+
+    this.submittedSuccessfully = true;
+  }
+
+  hideSuccess(): void {
+    this.submittedSuccessfully = false;
+  }
+}
--- src/app/sections/hero/hero.ts
+++ src/app/sections/hero/hero.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { YoutubeEmbed } from "../../components/youtube-embed/youtube-embed";
+import { ContactForm } from "../../components/contact-form/contact-form";
 
 @Component({
   selector: 'app-hero',
-  imports: [YoutubeEmbed],
+  imports: [YoutubeEmbed, ContactForm],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
