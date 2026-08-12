6e1b4b2c553259e62fb6193d6039f61ebd1a9469
cw <c.wagner.germany@gmail.com>
2026-08-12 11:58:27 +0200

    comment

--- src/app/sections/hero/hero.ts
+++ src/app/sections/hero/hero.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-hero',
@@ -6,4 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
-export class Hero {}
+export class Hero {
+  private sanitizer = inject(DomSanitizer);
+
+  readonly videoId = 'WYKIPCdAI1U';
+
+  isConsentGiven = signal(false);
+
+  embedUrl = computed<SafeResourceUrl>(() => {
+    const url = `https://www.youtube-nocookie.com/embed/${this.videoId}?autoplay=1`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  });
+
+  grantConsent(): void {
+    this.isConsentGiven.set(true);
+  }
+}
