29d0d8595783b289e4471072f765f1a7bd6caa91
cw <c.wagner.germany@gmail.com>
2026-08-23 17:06:00 +0200

    imprint data injection

--- src/app/components/legal/imprint/imprint.ts
+++ src/app/components/legal/imprint/imprint.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IMPRINT_DATA } from '../../../private-data.config';
 
 @Component({
   selector: 'app-imprint',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './imprint.html',
   styleUrls: ['../legal.scss', './imprint.scss'],
 })
-export class Imprint {}
+export class Imprint {
+  imprint = IMPRINT_DATA;
+}
